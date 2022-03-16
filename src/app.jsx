d9e40e31f0ca60b12e2f3304e840a047123bc75c
papatangosierra <pts@sockdolager.net>
2022-03-16 17:55:20 -0600

    script file loading added
    Very bare-bones, but it works. Needs polish but I'm counting the feature as written.

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import { ScriptPanel } from "./components/script-panel.jsx";
@@ -6,24 +6,27 @@ import { StyleMenuPanel } from "./components/setup-panel.jsx";
 import { SerifuDoc } from "./doc.js";
 import { serifuText } from "./components/test-data.js";
 
-// function importDataFromFile() {
-//   // let newScriptState
-//   console.log("attempting import...");
-//   const reader = new FileReader();
-//   const importFile = id("import").files[0]; // select the first file from the input widget
-//   let myDoc;
-//   console.log("file size: " + importFile.size);
-//   reader.onload = () => {
-//     // when file is loaded
-//     console.log("reader.onload triggered");
-//     myDoc = new SerifuDoc(reader.result);
-//     // have Indesign show an alert to grab focus
-//     // csInterface.evalScript('alert("Script data imported.")');
-//   };
-//   reader.readAsText(importFile);
-//   console.log("attempting to read as text");
-//   return myDoc;
-// }
+function importDataFromFile() {
+  let dataFilePath = window.cep.fs.showOpenDialogEx( // has to be "Ex" otherwise the commented-out
+    false,                                           // kludge down there is necessary (WOW!!!)
+    false,
+    "Select Serifu script file to load",
+    null,
+    ["txt", "srf", "serifu"]
+  ).data[0] // .slice(7).replace(/%20/g, " ");
+  console.log(`data file path is: ${dataFilePath}`)
+  let dataFile = window.cep.fs.readFile(dataFilePath);
+  if (dataFile.err === 0) {
+    return new SerifuDoc(dataFile.data);
+  } else {
+    console.log(JSON.stringify(dataFile, 0, 4));
+    return new SerifuDoc(`# Page 1
+    - 1
+    Serifu-chan: Looks like we're having some trouble opening the data file.
+
+    `);
+  }
+}
 
 export const theDoc = importDataFromFile();
 
